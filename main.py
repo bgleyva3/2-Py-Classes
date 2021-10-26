eaff8b580c625e17ecb3292ef8e563e8f3c0ac25
bgleyva3 <bgleyva3@gmail.com>
2021-10-26 03:25:15 -0500

    2

--- main.py
+++ main.py
@@ -1,56 +1,50 @@
-
-""" 
-    Se abre el archivo
-    Se lee linea por linea
-    Se crea un arreglo de palabras
-    La palabra de cada arreglo se mete a un objeto y suma 1
-    Al final se recorre todo el arreglo y se saca el valor y su key más grande
-"""
-
 import sys 
 
 file_name = input("Ingrese el archivo a abrir: ")
 
 try:
-    file = open(file_name)
+    file_selected = open(file_name)
 except OSError as e:
     print("No se encontró un archivo con el nombre", file_name)
     sys.exit()
 
-frequent_word = ""
-frequency = 0
-word_Obj = {}
-count = 0;
+class Counter:
+    @staticmethod
+    def most_repeated_words(file):
 
-for line in file:
-    line_word = line.strip().lower().replace(',','').replace('.','').split();
+        word_Obj = {}
+        array = []
 
-    for w in line_word:
-        word_Obj[w] = word_Obj.get(w, 0) + 1
+        for line in file:
+            line_word = line.strip().lower().replace(',','').replace('.','').split();
 
-
-#Creo un arreglo vacio 
-#Reviso la longitud del array
-#if len = 0, inserto el primer objeto
-#else, comparo el 
-
-
-for i in word_Obj:
-    print(i)
-    print(word_Obj[i])
-    print({i: word_Obj[i]})
-    print("--------------------------------")
-    """ count = word_Obj[i]
-    if(count > frequency):
-        frequency = count;
-        frequent_word = i """
-    
-
-""" print("------------------------------------")
-print("Palabra más repetida: " + frequent_word)
-print("Frecuencia: " + str(frequency))
-print("------------------------------------") """
-file.close();
+            for w in line_word:
+                word_Obj[w] = word_Obj.get(w, 0) + 1
 
 
 
+        for i in word_Obj:
+            if len(array) == 0:
+                array.append([i, word_Obj[i]])
+            else:
+                inserted = False
+                for j in range(len(array)):
+                    if word_Obj[i] > array[j][1]:
+                        array.insert(j, [i, word_Obj[i]])
+                        inserted = True
+                        break
+                if inserted == False and len(array) < 10:
+                    array.append([i, word_Obj[i]])
+                array = array[0:10]
+
+        print("The most repeated words are:")
+
+        count = 0
+        for i in array:
+            count = count + 1
+            print(count, ') "', i[0], '" ', i[1], " veces")
+
+
+Counter.most_repeated_words(file_selected)
+
+file_selected.close();
--- www.py
+++
@@ -1,5 +0,0 @@
-we = "we"
-array = [{"tu": 3}, {we: 2}]
-
-print(we)
-print(array)
